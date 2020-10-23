1c07d67d08a0b0937b1a9a8ae0801f0419b902b7
Tamir K. Yirga <tkifle@gmail.com>
2020-10-23 10:45:30 +0300

    added angular material and created a shared material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
